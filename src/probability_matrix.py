f8ad39a1c7a9cb19c99013544462921819669b5b
Laurin Brinkmann <laurin.brinkmann@gmail.com>
2021-03-03 12:12:10 +0100

    applies pep8 code style to matrix.py

--- src/probability_matrix.py
+++ src/probability_matrix.py
@@ -8,11 +8,15 @@ import utils
 dir_path = f'{utils.get_project_root()}/data/processed/'
 
 def concatenate_files(dir_path):
+
     '''dir_path is the path to the directory where are stored the cleaned
-       csv to be concatenated. The function concatenates the csv files and 
+       csv to be concatenated. The function concatenates the csv files and
        returns a DataFrame'''
     list_of_files = os.listdir(dir_path)
-    conc_df = pd.DataFrame({'customer_no':[], 'location_t':[], 'location_t+1':[]})
+    conc_df = pd.DataFrame({
+        'customer_no': [],
+        'location_t': [],
+        'location_t+1': []})
 
     for filename in list_of_files:
         filepath = dir_path + filename
@@ -21,11 +25,16 @@ def concatenate_files(dir_path):
         return conc_df
 
 def calculate_prob_matrix(conc_df):
-    '''conc_df is a DataFrame (must have state_t and state_t+1 states as columns). 
-        The function calclates and returns the probabily matrix'''
-    prob_matrix = pd.crosstab(conc_df['location_t'], conc_df['location_t+1'], normalize=0)
+
+    '''conc_df is a DataFrame (must have state_t and state_t+1 states as
+       columns). The function calclates and returns the probabily matrix'''
+    prob_matrix = pd.crosstab(
+        conc_df['location_t'],
+        conc_df['location_t+1'],
+        normalize=0)
     return prob_matrix
-    
+
+
 conc_df = concatenate_files(dir_path)
 prob_matrix = calculate_prob_matrix(conc_df)
 
