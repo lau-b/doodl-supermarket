6ade7ffc6a047962485a8de694f964a09a8ba6ec
Alessandro Cubeddu <alessandro.cubeddu@yahoo.co.uk>
2021-03-03 08:16:36 +0100

    Add is_active(), get_current_state() and get_previous_state()

--- src/Customer.py
+++ src/Customer.py
@@ -12,11 +12,12 @@ prob_matrix = calculate_prob_matrix(concatenate_files(dir_path))
 class Customer:
     '''single customer that can move in the supermarket randomly
        according to a probability matrix'''
-    def __init__(self, name, initial_state,  prob_matrix, budget = 100,):
+    def __init__(self, name, initial_state,  prob_matrix, budget = 100):
         self.name = name 
         self.state = initial_state 
         self.budget = budget 
         self.matrix = prob_matrix
+        self.previous = initial_state
 
     def __repr__(self):
         state = f'Customer {self.name} in state {self.state}.\n'
@@ -29,9 +30,24 @@ class Customer:
         values of the prob matrix '''
         prob_line = np.array([self.matrix.loc[self.matrix.index.get_level_values(0) == self.state]]).reshape(-1)
         next_st = np.random.choice(['checkout', 'dairy', 'drinks', 'fruit', 'spices'], p=prob_line)
-        #print(prob_line)
+        print(prob_line)
+        self.previous = self.state
         self.state = next_st
         return next_st
+
+    def is_active(self):
+        if (self.state == 'checkout') and (self.previous == self.state):
+            return False
+        else:
+            return True
+
+    def get_current_state(self):
+        return self.state
+
+    def get_previous_state(self):
+        return self.previous
+
+
         
 
 
@@ -40,8 +56,10 @@ cust1 = Customer('Alex', 'dairy', prob_matrix)
 while True:
     next = cust1.next_state()
     time.sleep(1)
-    print(next)
-    if next == 'checkout':
+    print('Is active?', cust1.is_active())
+    if cust1.get_current_state() == 'checkout' and cust1.get_previous_state() == 'checkout':
+        print('Previous state =', cust1.get_previous_state())
+        print('Current state =', cust1.get_current_state())
         break
 
         
